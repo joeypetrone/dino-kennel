7bb5bdcc8eb66a63135a42a483255392271d456b
Joey Petrone <joey.petrone@gmail.com>
2020-02-22 12:26:52 -0600

    Added single view

--- main.js
+++ main.js
@@ -40,9 +40,27 @@ const closeSingleViewEvent = () => {
     printDinos(dinos);
 };
 
-const viewSingleDino = () => {
+const viewSingleDino = (e) => {
+    const dinoId = e.target.closest('.card').id;
+    const selectedDino = dinos.find((x) => dinoId === x.id);
     let domString = '';
-    domString += '<button id="close-single-view" class="btn btn-outline-dark single-dino"><i class="fas fa-times-circle"></i></button>'
+    domString += '<div class="container card bg-light">';
+    domString += '   <div class="row">';
+    domString += '       <div class="col-6">';
+    domString += `               <img class="img-fluid" src="${selectedDino.imageUrl}" alt=""/>`;
+    domString += '       </div>';
+    domString += '       <div class="col-5">';
+    domString += `           <h2>${selectedDino.name}</h2>`;
+    domString += `           <p>Type: ${selectedDino.type}</p>`;
+    domString += `           <p>Age: ${selectedDino.age}</p>`;
+    domString += `           <p>Owner: ${selectedDino.owner}</p>`;
+    domString += `           <p>Health: ${selectedDino.health}</p>`;
+    domString += '       </div>'
+    domString += '       <div class="col-1">';
+    domString += '          <button id="close-single-view" class="btn btn-outline-dark single-dino"><i class="fas fa-times-circle"></i></button>';
+    domString += '       </div>'
+    domString += '</div>';
+
     printToDom('kennel', '');
     printToDom('single-view', domString);
     document.getElementById('close-single-view').addEventListener('click', closeSingleViewEvent);    
@@ -60,9 +78,9 @@ const printDinos = (dinoArray) => {
     let domString = '';
     for (let i = 0; i < dinoArray.length; i++) {
         domString += '<div class="col-4">';
-        domString += '<div class="card">';
+        domString += `<div id="${dinoArray[i].id}" class="card">`;
         domString += `<img class="card-img-top" src="${dinoArray[i].imageUrl}" alt="Card">`;
-        domString += '<div class="card-body">';
+        domString += '<div class="card-body text-center">';
         domString += `<h5 class="card-title">${dinoArray[i].name}</h5>`;
         domString += `<p class="card-text">Health: ${dinoArray[i].health}</p>`;
         domString += '<button class="btn btn-outline-dark single-dino"><i class="fas fa-eye"></i></button>'
